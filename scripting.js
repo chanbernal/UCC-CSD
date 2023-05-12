0b50c20a55c62e67c172663fcb9540593b15b1b1
chanbernal <124118547+chanbernal@users.noreply.github.com>
2023-05-12 21:28:58 +0800

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,81 @@
+let wrapperTed = document.getElementById('wrapperTed');
+let ted = document.getElementById('ted');
+ted.addEventListener("mouseover", popUpTed);
+ted.addEventListener("mouseout", popUpNoneTed);
+function popUpTed() {
+    wrapperTed.setAttribute("style", "display:block");
+}
+function popUpNoneTed() {
+ wrapperTed.style.display = "none";  
+}
+
+let wrapperVivian = document.getElementById('wrapperVivian');
+let vivian = document.getElementById('vivian');
+vivian.addEventListener("mouseover", popUpVivian);
+vivian.addEventListener("mouseout", popUpNoneVivian);
+function popUpVivian() {
+    wrapperVivian.setAttribute("style", "display:block");
+}
+function popUpNoneVivian() {
+    wrapperVivian.style.display = "none"; 
+}
+let wrapperGarrote = document.getElementById('wrapperGarrote');
+let garrote = document.getElementById('garrote');
+garrote.addEventListener("mouseover", popUpGarrote);
+garrote.addEventListener("mouseout", popUpNoneGarrote);
+function popUpGarrote() {
+    wrapperGarrote.setAttribute("style", "display:block");
+}
+function popUpNoneGarrote() {
+    wrapperGarrote.style.display = "none";
+}
+let wrapperJenna = document.getElementById('wrapperJenna');
+let jenna = document.getElementById('jenna');
+jenna.addEventListener("mouseover", popUpJenna);
+jenna.addEventListener("mouseout", popUpNoneJenna);
+function popUpJenna() {
+    wrapperJenna.setAttribute("style", "display:block");
+}
+function popUpNoneJenna() {
+    wrapperJenna.style.display = "none";  
+}
+let wrapperRalban = document.getElementById('wrapperRalban');
+let ralban = document.getElementById('ralban');
+ralban.addEventListener("mouseover", popUpRalban);
+ralban.addEventListener("mouseout", popUpNoneRalban);
+function popUpRalban() {
+    wrapperRalban.setAttribute("style", "display:block");
+}
+function popUpNoneRalban() {
+    wrapperRalban.style.display = "none";
+}
+let wrapperJoble = document.getElementById('wrapperJoble');
+let joble = document.getElementById('joble');
+joble.addEventListener("mouseover", popUpJoble);
+joble.addEventListener("mouseout", popUpNoneJoble);
+function popUpJoble() {
+    wrapperJoble.setAttribute("style", "display:block");
+}
+function popUpNoneJoble() {
+    wrapperJoble.style.display = "none";
+}
+let wrapperLalban = document.getElementById('wrapperLalban');
+let lalban = document.getElementById('lalban');
+lalban.addEventListener("mouseover", popUpLalban);
+lalban.addEventListener("mouseout", popUpNoneLalban);
+function popUpLalban() {
+    wrapperLalban.setAttribute("style", "display:block");
+}
+function popUpNoneLalban() {
+    wrapperLalban.style.display = "none";
+}
+let wrapperNievas = document.getElementById('wrapperNievas');
+let nievas = document.getElementById('nievas');
+nievas.addEventListener("mouseover", popUpNievas);
+nievas.addEventListener("mouseout", popUpNoneNievas);
+function popUpNievas() {
+    wrapperNievas.setAttribute("style", "display:block");
+}
+function popUpNoneNievas() {
+    wrapperNievas.style.display = "none"; 
+}
